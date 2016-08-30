050acdced04b8cd7c241aa08678664894eacd051
Frank <francoisbelanger1993@hotmail.com>
2016-08-30 13:41:04 -0400

    noticed a second cable.yml file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,13 +1,3 @@
 require 'redis'
-# url = Rails.application.config_for("redis/cable")['url']
-# encoded_url = URI.encode(url)
-# uri = URI.parse(encoded_url)
-# REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-REDIS = Redis.new(Rails.application.config_for("redis/cable"))
 
-# Test
-puts "\n" * 10
-REDIS.set("hello", "hello")
-puts REDIS.get("hello")
-REDIS.del("hello")
-puts "\n" * 10
+REDIS = Redis.new(Rails.application.config_for("redis/cable"))
