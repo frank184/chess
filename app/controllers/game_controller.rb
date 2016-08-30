ac3e2084505669c95b12643aab59e5217223c9a3
Frank <francoisbelanger1993@hotmail.com>
2016-08-30 00:49:47 -0400

    fixed communicating compiled paths to javascript library

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,18 @@
 class GameController < ApplicationController
   def play
+    gon.push({
+      bB: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/bB'),
+      bK: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/bK'),
+      bN: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/bN'),
+      bP: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/bP'),
+      bQ: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/bQ'),
+      bR: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/bR'),
+      wB: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/wR'),
+      wK: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/wK'),
+      wN: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/wN'),
+      wP: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/wP'),
+      wQ: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/wQ'),
+      wR: ActionController::Base.helpers.image_path('chessboard/chesspieces/wikipedia/wR')
+    })
   end
 end
