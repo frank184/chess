1b73af9f49233ac58e97d2f5e11061ff4d53d77d
Frank <francoisbelanger1993@hotmail.com>
2016-08-30 11:48:17 -0400

    encoded url -> uri

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
 require 'redis'
-
-REDIS = Redis.new(Rails.application.config_for("redis/cable"))
+url = Rails.application.config_for("redis/cable")['url']
+encoded_url = URI.encode(url)
+uri = URI.parse(encoded_url)
+REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+# REDIS = Redis.new(Rails.application.config_for("redis/cable"))
