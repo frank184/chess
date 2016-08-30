5b1e4c28b3fa75a391238d72f09c879856c183c2
Frank <francoisbelanger1993@hotmail.com>
2016-08-30 00:06:36 -0400

    recompiled assets

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Chess
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
