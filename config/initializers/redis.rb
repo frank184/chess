9a83dda809ce52f0f3d505bc7b0730161e98645f
Frank <francoisbelanger1993@hotmail.com>
2016-08-30 13:27:13 -0400

    test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,3 +4,10 @@ require 'redis'
 # uri = URI.parse(encoded_url)
 # REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 REDIS = Redis.new(Rails.application.config_for("redis/cable"))
+
+# Test
+puts "\n" * 10
+REDIS.set("hello", "hello")
+puts REDIS.get("hello")
+REDIS.del("hello")
+puts "\n" * 10
